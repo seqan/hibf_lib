74f835db739719eaa9dd5794d60f329637e87f50
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-06-25 20:40:15 +0200

    [MISC] Remove count.

--- src/chopper.cpp
+++ src/chopper.cpp
@@ -2,13 +2,12 @@
 #include <seqan3/core/debug_stream.hpp>
 
 #include <hibf/build/chopper_build.hpp>
-#include <hibf/count/chopper_count.hpp>
 #include <hibf/search/chopper_search.hpp>
 
 int main(int argc, const char *argv [])
 {
     seqan3::argument_parser top_level_parser{"chopper", argc, argv, seqan3::update_notifications::off,
-                                             {"count", "build", "search"}};
+                                             {"build", "search"}};
     top_level_parser.info.version = "1.0.0";
 
     try
@@ -23,9 +22,7 @@ int main(int argc, const char *argv [])
 
     seqan3::argument_parser & sub_parser = top_level_parser.get_sub_parser(); // hold a reference to the sub_parser
 
-    if (sub_parser.info.app_name == std::string_view{"chopper-count"})
-        return chopper_count(sub_parser);
-    else if (sub_parser.info.app_name == std::string_view{"chopper-build"})
+    if (sub_parser.info.app_name == std::string_view{"chopper-build"})
         return chopper_build(sub_parser);
     else if (sub_parser.info.app_name == std::string_view{"chopper-search"})
         return chopper_search(sub_parser);
